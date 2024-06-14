51aecb839fb23ec7c400a485b89db4e8179b41b8
apilatosba <aposdapulos098@outlook.com>
2024-06-14 18:59:49 +0300

    push

---
+++ Main/Program.cs
@@ -0,0 +1,45 @@
+﻿using DotGLFW;
+using System;
+
+using static DotGL.GL;
+
+namespace Main {
+   class Program {
+      static void Main() {
+         if (!Glfw.Init()) {
+            Console.Error.WriteLine("Failed to initialize GLFW");
+            Environment.Exit(1);
+         }
+
+         Glfw.SetErrorCallback((errorCode, description) => {
+            Console.Error.WriteLine($"GLFW Error: {errorCode} - {description}");
+         });
+
+         Glfw.WindowHint(Hint.ContextVersionMajor, GetProjectOpenGLVersionMajor());
+         Glfw.WindowHint(Hint.ContextVersionMinor, GetProjectOpenGLVersionMinor());
+         Glfw.WindowHint(Hint.OpenGLProfile, OpenGLProfile.CoreProfile);
+         Glfw.WindowHint(Hint.Samples, 4);
+
+         Window window = Glfw.CreateWindow(640, 480, "FeelsDankMan", Monitor.NULL, Window.NULL);
+
+         if (window == null) {
+            Console.Error.WriteLine("Failed to create GLFW window");
+            Glfw.Terminate();
+            Environment.Exit(1);
+         }
+
+         Glfw.MakeContextCurrent(window);
+         Import(Glfw.GetProcAddress);
+
+         Glfw.GetGamepadState(Joystick.Joystick1, out GamepadState state); // Crashes
+
+         while (!Glfw.WindowShouldClose(window)) {
+            Glfw.PollEvents();
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            Glfw.SwapBuffers(window);
+         }
+      }
+   }
+}
